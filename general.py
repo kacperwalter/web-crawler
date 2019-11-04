f68a4bae2363b9f0c6f41d309e3671f019589b03
Kacper <kacwal@poczta.fm>
2019-11-04 20:58:20 +0100

    created create_data_file funck and write_file func

--- general.py
+++ general.py
@@ -1,10 +1,29 @@
 import os
 
 # setting different "projects" for every different crawled website
-
 def create_project_dir(directory):
     if not os.path.exists(directory):
         print('Creating project ' + directory)
         os.mkdir(directory)
 
-create_project_dir('kacper')
+
+# creating queue and crawled files (if not crawled)
+def create_data_files(project_name, base_url):
+    queue = project_name + 'queue.txt' #queue.txt => list on links that will be creawled
+    crawled = project_name + 'crawled.txt'
+
+    #checking if file exist before we create them
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '') # creating empty file
+
+# creating new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.writable(data)
+    f.close()
+
+
+
+
