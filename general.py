024db7b7ef6274e6672f7131a625b2a2d9e3cb12
Kacper <kacwal@poczta.fm>
2019-11-05 20:58:06 +0100

    made append_to_file and delete_file_contents funcs

--- general.py
+++ general.py
@@ -6,11 +6,10 @@ def create_project_dir(directory):
         print('Creating project ' + directory)
         os.mkdir(directory)
 
-
 # creating queue and crawled files (if not crawled)
 def create_data_files(project_name, base_url):
-    queue = project_name + 'queue.txt' #queue.txt => list on links that will be creawled
-    crawled = project_name + 'crawled.txt'
+    queue = project_name + '/queue.txt' #queue.txt => list on links that will be creawled
+    crawled = project_name + '/crawled.txt'
 
     #checking if file exist before we create them
     if not os.path.isfile(queue):
@@ -18,12 +17,18 @@ def create_data_files(project_name, base_url):
     if not os.path.isfile(crawled):
         write_file(crawled, '') # creating empty file
 
-# creating new file
+# creating new file ('w' is write mode)
 def write_file(path, data):
     f = open(path, 'w')
-    f.writable(data)
+    f.write(data)
     f.close()
 
+# adding data onto an existing file ('a' means append file)
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
 
-
-
+# delete the contents of file
+def delete_file_contents(path):
+    with open(path, 'w'):
+        pass
